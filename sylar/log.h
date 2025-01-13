4ede9c05a3fe62a25388d0fcad42a29e29b65125
xhsct <1577743956@qq.com>
2025-01-13 20:24:03 +0800

    0113

--- sylar/log.h
+++ sylar/log.h
@@ -38,6 +38,8 @@
 #define SYLAR_LOG_FMT_ERROR(logger, fmt, ...) SYLAR_LOG_FMT_LEVEL(logger, sylar::LogLevel::ERROR, fmt, __VA_ARGS__)
 #define SYLAR_LOG_FMT_FATAL(logger, fmt, ...) SYLAR_LOG_FMT_LEVEL(logger, sylar::LogLevel::FATAL, fmt, __VA_ARGS__)
 
+#define SYLAR_LOG_ROOT() sylar::LoggerMgr::GetInstance()->getRoot()
+
 namespace sylar{
 class Logger;
 
@@ -173,6 +175,7 @@ public:
     LoggerManager();
     Logger::ptr getLogger(const std::string name);
     void init();
+    Logger::ptr getRoot(){return m_root;}
 private:
     std::map<std::string,Logger::ptr> m_logger;
     Logger::ptr m_root;
